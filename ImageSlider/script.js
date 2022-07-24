a4c70d2080649508c24244034f9ded9c3ddf4cee
supradipdas02 <supradipdas02@gmail.com>
2022-07-24 20:09:34 +0530

    Updates

--- ImageSlider/script.js
+++ ImageSlider/script.js
@@ -5,11 +5,11 @@ document.getElementById("left-arrow").addEventListener("click", function () {
   if (counter < 1) {
     counter = 4;
   }
-  console.log(
-    "left arrow clicked" +
-    " " +
-    document.getElementById("radio" + counter).checked
-  );
+  // console.log(
+  //   "left arrow clicked" +
+  //   " " +
+  //   document.getElementById("radio" + counter).checked
+  // );
   document.getElementById("radio" + counter).checked = true;
 });
 document.getElementById("right-arrow").addEventListener("click", function () {
@@ -17,12 +17,12 @@ document.getElementById("right-arrow").addEventListener("click", function () {
   if (counter > 4) {
     counter = 1;
   }
-  console.log(
-    "Right arrow clicked" +
-    " " +
-    counter +
-    " " +
-    document.getElementById("radio" + counter).checked
-  );
+  // console.log(
+  //   "Right arrow clicked" +
+  //   " " +
+  //   counter +
+  //   " " +
+  //   document.getElementById("radio" + counter).checked
+  // );
   document.getElementById("radio" + counter).checked = true;
 });
